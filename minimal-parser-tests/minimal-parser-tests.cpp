84c85573407f1b91d2f57308f6c9beca7ff00236
Cristobal Vergara Niedermayr <cristobal.vergara.niedermayr@gmail.com>
2015-11-25 12:55:45 -0800

    Reading files with test data for tests.

--- minimal-parser-tests/minimal-parser-tests.cpp
+++ minimal-parser-tests/minimal-parser-tests.cpp
@@ -1,9 +1,54 @@
-#include <CppUTest/TestHarness.h>
-#include <CppUTest/CommandLineTestRunner.h>
+#include <fstream>      // std::ifstream
+#include <iostream>     // std::cout
+
 #include "../minimal-parser/minimal-parser.h"
 
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+
+using namespace std;
+
+void readFile(const char* filename, char** buffer);
+
+const char* testDataAlignedFileName = "test-data-aligned.txt";
+char* testDataAlignedBuffer;
+
+const char* testDataNotAlignedFileName = "test-data-not-aligned.txt";
+char* testDataNotAlignedBuffer;
+
 TEST_GROUP(FindGPRMC) {
+    void setup() {
+        readFile(testDataAlignedFileName, &testDataAlignedBuffer);
+        cout << "testDataAlignedBuffer:" << endl << testDataAlignedBuffer << endl;
+        readFile(testDataNotAlignedFileName, &testDataNotAlignedBuffer);
+        cout << "testDataNotAlignedBuffer:" << endl << testDataNotAlignedBuffer << endl;
+    }
+
+    void teardown() {
+        delete[] testDataAlignedBuffer;
+        delete[] testDataNotAlignedBuffer;
+    }
 };
+
+void readFile(const char* filename, char** buffer) {
+    ifstream inputFile(filename, ifstream::binary);
+    if (!inputFile) {
+        FAIL("Unable to open input file.");
+    }
+    inputFile.seekg(0, inputFile.end);
+    long length = inputFile.tellg();
+    inputFile.seekg(0, inputFile.beg);
+    
+    *buffer = new char[length];
+
+    inputFile.read(*buffer, length);
+
+    if(!inputFile) {
+        FAIL("Failed to read file.");
+    }
+
+    inputFile.close();
+}
 
 TEST(FindGPRMC, Find1) {
     char test[7];
--- minimal-parser/minimal-parser.c
+++ minimal-parser/minimal-parser.c
@@ -2,9 +2,9 @@
 
 STATUS parse(char* data) {
     return 0;
-};
+}
 
 STATUS getData(struct min_parser_data* data) {
     return 0;
-};
+}
 
